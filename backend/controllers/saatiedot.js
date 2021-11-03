d5be5a07db345e5b056b0148b8b988a43c517768
mikkskxt <santeri.m.mikkonen@student.jyu.fi>
2021-11-03 12:20:45 +0200

    Säätietojen hakeminen

--- backend/controllers/saatiedot.js
+++ backend/controllers/saatiedot.js
@@ -1,13 +1,20 @@
-var Metolib = require('@fmidev/metolib')
+var Metolib = require('@fmidev/metolib') // "couldn't find declaration file"
+
+/* get('/:bbox', async (request, response) => {
+    bbox = request.params.bbox
+    // hae parserilta data valitulla sijainnilla
+}) */
+
 const url = 'http://opendata.fmi.fi/wfs'
 const query = 'fmi::forecast::hirlam::surface::point::simple'
 // asynkronisesti pyytää XML datan ja vastaanottaa sen parsettuna JSON:ksi
+// objekti parserin sisällä erilliseksi muuttujaksi, että arvoihin päästään käsiksi?
 var parser = new Metolib.WfsRequestParser()
 parser.getData({
     url: url,
     storedQueryId: query,
     requestParameter : 'Temperature,WeatherSymbol3,WindSpeedMS',
-    // timestep : 60 * 60 * 1000, jos haluaa esim 20 min välein
+    timestep : 20,
     // bbox : "min lat,min lon,max lat,max lon", tarkka sijainti kartalta
     sites: 'Jyväskylä', // vaihdetaan bbox kun bbox toteutus on tiedossa
     callback : function(data) {
